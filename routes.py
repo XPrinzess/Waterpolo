167b0ec973a5050236330d64b14639d590dfa4ed
XPrinzess <xenia.prinz2018@gmail.com>
2023-08-21 14:40:54 +1200

    Worked on queries for individual tournament page
    Also worked on formatting according to pep8 (lines were too long)

--- routes.py
+++ routes.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html", title="Waterpolo Players")  # title variable names the tab in the website
+    return render_template("home.html", title="Waterpolo Players")
+    # title variable names the tab in the website
 
 
 @app.route('/countries')
@@ -21,7 +22,8 @@ def countries():
     cur = conn.cursor()
     cur.execute('SELECT * FROM Country;')
     countries = cur.fetchall()
-    return render_template("countries.html", countries=countries, title="Waterpolo Players")
+    return render_template("countries.html", countries=countries, 
+                           title="Waterpolo Players")
 
 
 @app.route('/country/<int:id>')
@@ -30,31 +32,46 @@ def indiv_country(id):
     cur = conn.cursor()
     cur.execute('SELECT * FROM Country WHERE id = ?', (id,))
     country = cur.fetchall()
-    cur.execute('SELECT first_name, last_name, image, national_team, player.id FROM Player WHERE country_id = ? AND national_team = 1', (id,))
+    cur.execute('SELECT first_name, last_name, image, national_team, player.id FROM Player \
+                WHERE country_id = ? AND national_team = 1', (id,))
     national_team = cur.fetchall()
-    cur.execute('SELECT first_name, last_name, image, national_team, player.id FROM Player WHERE country_id = ? AND national_team = 0', (id,))
+    cur.execute('SELECT first_name, last_name, image, national_team, player.id FROM Player \
+                WHERE country_id = ? AND national_team = 0', (id,))
     former_team = cur.fetchall()
     #get template to only print 'former team' or 'national team' once
-    return render_template("country.html", country=country, national_team=national_team, former_team=former_team, title="Waterpolo Players")
+    return render_template("country.html", country=country, national_team=national_team, 
+                           former_team=former_team, title="Waterpolo Players")
 
 
 @app.route('/players')
 def players():
     conn = sqlite3.connect('waterpolo.db')
     cur = conn.cursor()
-    cur.execute('SELECT Player.id, Player.first_name, Player.last_name, Player.image, Player.world_ranking, Country.flag FROM Player Join Country ON Player.country_id=Country.id WHERE Player.world_ranking IS NOT NULL;')
+    cur.execute('SELECT Player.id, Player.first_name, Player.last_name, \
+                Player.image, Player.world_ranking, Country.flag FROM Player \
+                Join Country ON Player.country_id=Country.id \
+                WHERE Player.world_ranking IS NOT NULL;')
     ranked_players = cur.fetchall()
-    cur.execute('SELECT Player.id, Player.first_name, Player.last_name, Player.image, Country.flag FROM Player Join Country ON Player.country_id=Country.id WHERE Player.world_ranking IS NULL;')
+    cur.execute('SELECT Player.id, Player.first_name, Player.last_name, \
+                Player.image, Country.flag FROM Player \
+                Join Country ON Player.country_id=Country.id \
+                WHERE Player.world_ranking IS NULL;')
     players = cur.fetchall()
     # world ranking still required in further iterations (could have interaction from the user how they want the data ordered)
-    return render_template("players.html", ranked_players=ranked_players, players=players, title="Waterpolo Players")
+    return render_template("players.html", ranked_players=ranked_players, 
+                           players=players, title="Waterpolo Players")
 
 
 @app.route('/player/<int:id>')
 def indiv_player(id):
     conn = sqlite3.connect('waterpolo.db')
     cur = conn.cursor()
-    cur.execute('SELECT Player.first_name, Player.last_name, Player.image, Player.weight, Player.height, Player.world_ranking, Player.cap_number, Country.country, Country.flag, Player.right_hand_dominant, Player.left_hand_dominant, Player.national_team, Country.id FROM Player JOIN Country ON Player.country_id=Country.id WHERE Player.id = ?', (id,))
+    cur.execute('SELECT Player.first_name, Player.last_name, Player.image, \
+                Player.weight, Player.height, Player.world_ranking, Player.cap_number, \
+                Country.country, Country.flag, Player.right_hand_dominant, \
+                Player.left_hand_dominant, Player.national_team, Country.id FROM Player \
+                JOIN Country ON Player.country_id=Country.id \
+                WHERE Player.id = ?', (id,))
     player = cur.fetchall()
     return render_template("player.html", player=player, title="Waterpolo Players")
 
@@ -65,21 +82,35 @@ def tournaments():
     cur = conn.cursor()
     cur.execute('SELECT DISTINCT name_id FROM Tournament')
     tid = cur.fetchall()
-    tournaments =[]
+    tournaments = []
     for name in tid:
-        cur.execute('SELECT Tournament.year, TournamentName.tournament_name, TournamentName.description, TournamentName.logo, Tournament.id FROM Tournament JOIN TournamentName ON Tournament.name_id=TournamentName.id WHERE Tournament.name_id=?', (name))
+        cur.execute('SELECT Tournament.year, TournamentName.tournament_name, \
+                    TournamentName.description, TournamentName.logo, Tournament.id FROM Tournament \
+                    JOIN TournamentName ON Tournament.name_id=TournamentName.id \
+                    WHERE Tournament.name_id=?', (name))
         tournament = cur.fetchall()
         tournaments.append(tournament)
-    return render_template("tournaments.html", tournaments=tournaments, title="Waterpolo Players")
+    return render_template("tournaments.html", tournaments=tournaments, 
+                           title="Waterpolo Players")
 
 
 @app.route('/tournament/<int:name_id>')
-def indiv_tournament(name_id):
+def indiv_tournament(name_id, year):
     conn = sqlite3.connect('waterpolo.db')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM Tournament WHERE name_id = ?', (name_id,))
-    year = cur.fetchall()
-    return render_template("tournament.html", year=year, title="Waterpolo Players")
+    cur.execute('SELECT TournamentName.tournament_name, TournamentName.description, TournamentName.logo, \
+                Tournament.name_id, Tournament.year, Tournament.matches FROM Tournament \
+                JOIN TournamentName ON Tournament.name_id=TournamentName.id \
+                WHERE Tournament.name_id = ? AND Tournament.year = ?', (name_id, year,))
+    event = cur.fetchall()
+    cur.execute('SELECT id, country, flag FROM COUNTRY \
+                JOIN Country ON CountryTournament.country_id=Country.id \
+                JOIN Tournament ON CountryTournament.tournament_id=Tournament.id \
+                WHERE Tournament.name_id = ? AND Tournament.year = ?', (name_id, year,))
+    teams = cur.fetchall()
+    return render_template("tournament.html", event=event, teams=teams, 
+                           title="Waterpolo Players")
+# Check all the queries work change the variables for the ?'s so that it works
 
 
 @app.route('/positions')
@@ -95,11 +126,16 @@ def positions():
 def indiv_position(id):
     conn = sqlite3.connect('waterpolo.db')
     cur = conn.cursor()
-    cur.execute('SELECT Position.position, Position.image, Position.pos_copyright FROM Position WHERE Position.id = ?', (id,))
+    cur.execute('SELECT Position.position, Position.image, Position.pos_copyright FROM Position \
+                WHERE Position.id = ?', (id,))
     position = cur.fetchall()
-    cur.execute('SELECT Player.first_name, Player.last_name, Player.image, Country.flag, Player.id FROM PlayerPosition Join Player ON PlayerPosition.player_id=Player.id Join Country ON Player.country_id=Country.id Join Position ON PlayerPosition.position_id=Position.id WHERE Position.id = ?', (id,))
+    cur.execute('SELECT Player.first_name, Player.last_name, Player.image, Country.flag, Player.id FROM PlayerPosition \
+                Join Player ON PlayerPosition.player_id=Player.id \
+                Join Country ON Player.country_id=Country.id \
+                Join Position ON PlayerPosition.position_id=Position.id WHERE Position.id = ?', (id,))
     position_players = cur.fetchall()
-    return render_template("position.html", position=position, position_players=position_players, title="Waterpolo Players")
+    return render_template("position.html", position=position, position_players=position_players, 
+                           title="Waterpolo Players")
 
 
 if __name__ == "__main__":
